f76526dd9c061877fef4ba3ba148d7fa395e23fd
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2023-02-24 09:06:00 +0100

    ...

--- editor/neovim/nvim/init.lua
+++ editor/neovim/nvim/init.lua
@@ -633,8 +633,29 @@ require("lazy").setup(
             },
         },
 
+        -- { -- file switcher
+        --     "ThePrimeagen/harpoon",
+        --     lazy = true,
+        --     keys = {
+        --         { "<leader>ta", "<CMD> lua require(\"harpoon.mark\").add_file() <CR>", desc = "add current file" },
+        --         { "<leader>ti", "<CMD> lua require(\"harpoon.ui\").toggle_quick_menu() <CR>" },
+        --         { "<leader>1", "<CMD> lua require(\"harpoon.ui\").nav_file(1) <CR>" },
+        --         { "<leader>2", "<CMD> lua require(\"harpoon.ui\").nav_file(2) <CR>" },
+        --         { "<leader>3", "<CMD> lua require(\"harpoon.ui\").nav_file(3) <CR>" },
+        --         { "<leader>4", "<CMD> lua require(\"harpoon.ui\").nav_file(4) <CR>" },
+        --         { "<leader>t1", "<CMD> vsplit lua require(\"harpoon.mark\").get_marked_file(1).filename <CR>" },
+        --     },
+        --     dependencies = {
+        --         "nvim-telescope/telescope.nvim",
+        --         "nvim-lua/plenary.nvim",
+        --     },
+        --     config = function()
+        --         require("telescope").load_extension("harpoon")
+        --     end,
+        -- },
+
         { -- file switcher
-            "ThePrimeagen/harpoon",
+            "Vincent-von-Schmidt/harpoon",
             lazy = true,
             keys = {
                 { "<leader>ta", "<CMD> lua require(\"harpoon.mark\").add_file() <CR>", desc = "add current file" },
