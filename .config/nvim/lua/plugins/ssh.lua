773333a1a7376badacd59cca8273b1da2adb2e19
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2024-02-10 13:42:16 +0100

    feature: ssh delete mount point dir

--- .config/nvim/lua/plugins/ssh.lua
+++ .config/nvim/lua/plugins/ssh.lua
@@ -7,7 +7,13 @@ return {
     lazy = true,
     cmd = "RemoteSSHFSConnect",
     config = function()
-        require("remote-sshfs").setup({})
+        require("remote-sshfs").setup({
+            handlers = {
+                on_disconnect = {
+                    clean_mount_folders = true,
+                },
+            },
+        })
         require("telescope").load_extension("remote-sshfs")
     end,
 }
